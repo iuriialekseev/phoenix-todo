d6eecc597355bbbb5464471da979739b9fd02a76
Yuriy Alekseyev <dev.yuriy.a@gmail.com>
2020-07-15 12:59:30 +0300

    Revert "Remove tests"
    This reverts commit 37e08ec9316d3896d06cbee0fe223a3eb4b6af1c.

---
+++ test/phoenix_todo/tasks_test.exs
@@ -0,0 +1,64 @@
+defmodule PhoenixTodo.TasksTest do
+  use PhoenixTodo.DataCase
+
+  alias PhoenixTodo.Tasks
+
+  describe "tasks" do
+    alias PhoenixTodo.Tasks.Task
+
+    @valid_attrs %{name: "some name"}
+    @update_attrs %{name: "some updated name"}
+    @invalid_attrs %{name: nil}
+
+    def task_fixture(attrs \\ %{}) do
+      {:ok, task} =
+        attrs
+        |> Enum.into(@valid_attrs)
+        |> Tasks.create_task()
+
+      task
+    end
+
+    test "list_tasks/0 returns all tasks" do
+      task = task_fixture()
+      assert Tasks.list_tasks() == [task]
+    end
+
+    test "get_task!/1 returns the task with given id" do
+      task = task_fixture()
+      assert Tasks.get_task!(task.id) == task
+    end
+
+    test "create_task/1 with valid data creates a task" do
+      assert {:ok, %Task{} = task} = Tasks.create_task(@valid_attrs)
+      assert task.name == "some name"
+    end
+
+    test "create_task/1 with invalid data returns error changeset" do
+      assert {:error, %Ecto.Changeset{}} = Tasks.create_task(@invalid_attrs)
+    end
+
+    test "update_task/2 with valid data updates the task" do
+      task = task_fixture()
+      assert {:ok, %Task{} = task} = Tasks.update_task(task, @update_attrs)
+      assert task.name == "some updated name"
+    end
+
+    test "update_task/2 with invalid data returns error changeset" do
+      task = task_fixture()
+      assert {:error, %Ecto.Changeset{}} = Tasks.update_task(task, @invalid_attrs)
+      assert task == Tasks.get_task!(task.id)
+    end
+
+    test "delete_task/1 deletes the task" do
+      task = task_fixture()
+      assert {:ok, %Task{}} = Tasks.delete_task(task)
+      assert_raise Ecto.NoResultsError, fn -> Tasks.get_task!(task.id) end
+    end
+
+    test "change_task/1 returns a task changeset" do
+      task = task_fixture()
+      assert %Ecto.Changeset{} = Tasks.change_task(task)
+    end
+  end
+end
